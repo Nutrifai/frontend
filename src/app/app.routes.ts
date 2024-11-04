5d0931cd3780cc02c3f2483127c3f4306b164bf8
Haron-Terentin <haron.terentin@gmail.com>
2024-11-04 20:18:44 -0300

    Create Reset Pass / Clean Code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,16 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { AssinaturaComponent } from './assinatura/assinatura.component';
 import { DietComponent } from './diet/diet.component';
 import { NutricionistaComponent } from './nutricionista/nutricionista.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'diet', component: DietComponent },
+    { path: 'forget_password', component: ForgetPasswordComponent },
     { path: 'nutricionista', component: NutricionistaComponent },
     { path: 'login', component: LoginComponent },
     { path: 'cadastro', component: CadastroComponent },
     { path: 'assinatura', component: AssinaturaComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redireciona para 'home' por padrão
-    { path: '**', redirectTo: '/home' } // Captura rotas inválidas
+    //{ path: '', redirectTo: '/nutricionista', pathMatch: 'full' }, // Redireciona para 'home' por padrão
+    //{ path: '**', redirectTo: '/home' } // Captura rotas inválidas
 ];
